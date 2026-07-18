f54d6b72d1a70b2ea64914cb7868e82438283d99
Cole Seguin <hyacinthoyup@gmail.com>
2026-07-18 13:42:35 -0600

    Added main startup logic

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,23 @@ mod app;
 
 #[tokio::main]
 async fn main() {
+    // Get key and init state
     let api_key = std::env::var("GATEWAY_API_KEY").unwrap_or_else(|_| "dev_key".into());
     let state = AppState::new(api_key);
 
+    // Crate app instance an add routes
     let app = Router::new()
         .route("/health", get(health))
         .route("/execute", post(execute))
         .with_state(state);
+
+    // Bind listener to port 8080
+    let address = "0.0.0.0:8080";
+    let listener = tokio::net::TcpListener::bind(address).await.unwrap();
+    println!("Listening on 8080");
+
+    // Start server with axum gateway
+    axum::serve(listener, app).await.unwrap();
 }
 
 async fn health() -> &'static str {
