f239ae9f47af2c3c106ffe8238a9955349470598
Cole Seguin <hyacinthoyup@gmail.com>
2026-07-18 15:29:45 -0600

    started work on piston structs

--- src/piston.rs
+++ src/piston.rs
@@ -1,1 +1,28 @@
+use serde::{Deserialize, Serialize};
 
+#[derive(Serialize)]
+pub struct PistonFile {
+    content: String,
+}
+
+#[derive(Serialize)]
+pub struct PistonRequest {
+    language: String,
+    version: String,
+    files: Vec<PistonFile>,
+    stdin: String,
+    run_timout: u32,
+    run_memory_limit: i64,
+}
+
+#[derive(Deserialize)]
+pub struct PistonRun {
+    stdout: String,
+    stderr: String,
+    code: Option<u32>,
+}
+
+#[derive(Deserialize)]
+pub struct PistonReponse {
+    run: PistonRun,
+}
